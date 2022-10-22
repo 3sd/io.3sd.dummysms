a4c430714e403833f69d75a547ada91df26ce3a4
demeritcowboy <demeritcowboy@hotmail.com>
2022-10-22 11:27:42 -0400

    civix upgrade

--- dummysms.php
+++ dummysms.php
@@ -12,15 +12,6 @@ function dummysms_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function dummysms_civicrm_xmlMenu(&$files) {
-  _dummysms_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -122,54 +113,6 @@ function dummysms_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
   return _dummysms_civix_civicrm_upgrade($op, $queue);
 }
 
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function dummysms_civicrm_managed(&$entities) {
-  _dummysms_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function dummysms_civicrm_caseTypes(&$caseTypes) {
-  _dummysms_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
- */
-function dummysms_civicrm_angularModules(&$angularModules) {
-  _dummysms_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function dummysms_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _dummysms_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
 // --- Functions below this ship commented out. Uncomment as required. ---
 
 /**
@@ -177,9 +120,8 @@ function dummysms_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_preProcess
  *
-function dummysms_civicrm_preProcess($formName, &$form) {
 
-} // */
+ // */
 
 /**
  * Implements hook_civicrm_navigationMenu().
@@ -197,3 +139,12 @@ function dummysms_civicrm_navigationMenu(&$menu) {
   ));
   _dummysms_civix_navigationMenu($menu);
 } // */
+
+// /**
+//  * Implements hook_civicrm_entityTypes().
+//  *
+//  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
+//  */
+// function dummysms_civicrm_entityTypes(&$entityTypes) {
+//   _dummysms_civix_civicrm_entityTypes($entityTypes);
+// }
